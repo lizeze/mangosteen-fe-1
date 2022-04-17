f9281fe4d5c400050e6949f4279a92287fd17012
zeze.li <lizeze.boy@gmail.com>
2022-04-17 21:52:19 +0800

    添加jsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { defineComponent, ref } from "vue";
+
+export const App = defineComponent({
+    setup() {
+        const count = ref(0);
+        const onClick = () => {
+            count.value++;
+        }
+        return () => <>
+            <div>
+                {count.value}
+            </div>
+            <div>
+                <button onClick={onClick}>添加</button>
+            </div>
+            </>
+       
+    }
+});
--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  {{count}}
-</template>
-<script  setup lang="ts">import { ref } from 'vue';
-
-  
-  const  count=ref(0)
-</script>
